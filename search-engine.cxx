7fc0a46b6dc2de676c8192d2ee09100945968f48
harastw <94912005+harastw@users.noreply.github.com>
2023-02-13 18:16:35 +0700

    Update search-engine.cxx

--- search-engine.cxx
+++ search-engine.cxx
@@ -1,12 +1,13 @@
-#include "ConvertJSON.cpp"
+#include "ConvertJSON.cxx"
 
 int main()
 {
   ConvertJSON search_engine;
   search_engine.GetTextDocuments();
+  search_engine.printAllWords()
   search_engine.GetResponsesLimit();
   search_engine.GetRequests();
-  search_engine.LoadUnique();
+  search_engine.InvertedIndex();
   search_engine.PutAnswers(); 
   return 0;
 }

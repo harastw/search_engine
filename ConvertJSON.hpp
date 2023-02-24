c339f98a014b3805b0bc2406226753be7a084f29
harastw <94912005+harastw@users.noreply.github.com>
2023-02-24 19:50:11 +0700

    Update ConvertJSON.hpp

--- ConvertJSON.hpp
+++ ConvertJSON.hpp
@@ -1,25 +1,26 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 #include <fstream>
+
 #include <nlohmann/json.hpp>
+
 #define Entry std::pair<int,int> // docid, amount entry's
-using namespace std;
 
 class ConvertJSON {
-public:
-  void printAllWords();
-  void CheckWordsInDoc(int docid);
-  bool InvertedIndex();
+public:  
+  void CheckWordsInDoc(std::map<int,std::vector<std::string>>::iterator it);
+  bool ToUnique();
   bool GetTextDocuments();
   bool GetResponsesLimit();
   bool GetRequests();
   bool PutAnswers();
 private:
   int totalFiles, responsesLimit;
-  vector<string> requests;
-  map<string, vector<Entry>> uniqueWords; // word, vector docid
-  map<int, vector<string>> allWords; // docid, all words
+  std::vector<std::string> requests;
+  std::map<std::string, std::vector<Entry>> uniqueWords;
+  std::map<int, std::vector<std::string>> allWords;
 };

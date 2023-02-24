0e575c5e954f99aded4a70d6802fa46b63ce3c23
harastw <94912005+harastw@users.noreply.github.com>
2023-02-24 19:51:41 +0700

    Update search-engine.cxx

--- search-engine.cxx
+++ search-engine.cxx
@@ -1,13 +1,19 @@
 #include "ConvertJSON.cxx"
 
-int main()
-{
+int main() {
   ConvertJSON search_engine;
   search_engine.GetTextDocuments();
-  search_engine.printAllWords()
   search_engine.GetResponsesLimit();
-  search_engine.GetRequests();
-  search_engine.InvertedIndex();
-  search_engine.PutAnswers(); 
+  search_engine.ToUnique();
+
+  std::string choise;
+  while (true) {
+    search_engine.GetRequests();
+    search_engine.PutAnswers();
+    std::cout << "read requests again? y-yes/enything-no: ";
+    std::cin >> choise;
+    if (choise != "y" and choise != "yes")
+      break;
+  }
   return 0;
 }

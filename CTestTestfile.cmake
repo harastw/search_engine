4e2fc75f025a2764f3e3c7928a26f1e78672535c
name <uitrinken1@gmail.com>
2023-04-13 17:50:26 +0700

    remove garbage v2

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sark/code/cxx/search_engine
-# Build directory: /home/sark/code/cxx/search_engine
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("nlohmann_json")
-subdirs("Google_tests/lib")
